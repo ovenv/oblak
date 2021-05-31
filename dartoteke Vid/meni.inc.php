7b0042e767f461667094c9e15476e53567caf931
ovenv <77193250+ovenv@users.noreply.github.com>
2021-05-31 21:23:00 +0200

    Add files via upload

---
+++ dartoteke Vid/meni.inc.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <title>PHP: Include</title>
+</head>
+
+<body>
+<!--<div class="container">
+            <nav>
+                <ul>
+                    <li>O πJU
+                        <ul class="sub-nav">
+                            <li>ZGODOVINA</li>
+                            <li>UPORABA</li>
+                            <li>PRAKTIČNI PRIKAZ</li>
+                        </ul></li>
+                    </li>
+                    <li>ALGORITMI ZA IZRAČUN π</li>
+                    <li>APLIKACIJA</li>
+                    <li>πTA </li>
+                </ul>
+            </nav>
+            <div class="header"></div>
+        </div>-->
+
+        <h1>kwufhksdhfioahf</h1>
